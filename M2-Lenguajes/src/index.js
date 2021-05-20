b6ab7042f8fac9254adf213419d720f602d81790
Eric <rc84.eric@gmail.com>
2021-05-20 16:15:06 +0200

    syntax fix

--- M2-Lenguajes/src/index.js
+++ M2-Lenguajes/src/index.js
@@ -2,24 +2,20 @@
 // Head
 // Implementa una función head (inmutable), tal que, dado un array como entrada extraiga y devuelva su primer elemento. Uliza destructuring.
 
-const head = ([first, ...rest]) => {
-    return first;
-};
+const head = ([first, ...rest]) => first;
 
 const arr1 = [1, 2, 3];
 
-console.log(`1.-Array Head of [${arr1}] is: [${head(arr1)}]`);
+console.log(`1.-Array Head of [${arr1}] is: [${head(arr1)}]`);  //[1]
 
 // Tail
 // Implementa una función tail (inmutable), tal que, dado un array como entrada
 // devuelta todos menos el primer elemento. Uliza rest operator.
-const tail = ([first, ...rest]) => {
-    return rest;
-};
+const tail = ([first, ...rest]) => rest;
 
 const arr2 = [1, 2, 3];
 
-console.log(`1.-Array Tail of [${arr2}] is: [${tail(arr2)}]`);
+console.log(`1.-Array Tail of [${arr2}] is: [${tail(arr2)}]`); //[2,3]
 
 // Init
 // Implementa una función init (inmutable), tal que, dado un array como entrada
@@ -33,19 +29,16 @@ const init = ([...arr]) => {
 
 const arr3 = [1, 2, 3];
 
-console.log(`1.-Array Init of [${arr3}] is: [${init(arr3)}]`);
+console.log(`1.-Array Init of [${arr3}] is: [${init(arr3)}]`); //[1,2]
 
 // Last
 // Implementa una función last (inmutable), tal que, dado un array como entrada
 // devuelva el úlmo elemento.
-const last = ([...array]) => {
-    return array[array.length - 1];
-
-};
+const last = ([...array]) => array[array.length - 1];
 
 const arr4 = [1, 2, 3];
 
-console.log(`1.-Array Last of [${arr4}] is: [${last(arr4)}]`);
+console.log(`1.-Array Last of [${arr4}] is: [${last(arr4)}]`); //[3]
 
 // 2. Concat
 // Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada,
@@ -58,7 +51,7 @@ const concat = (a, b) => {
 const arr5 = [1, 2, 3];
 const arr6 = ["uno", "dos", "tres"];
 
-console.log(`2.-Concat of [${arr5}]+[${arr6}]= [${concat(arr5, arr6)}]`);
+console.log(`2.-Concat of [${arr5}]+[${arr6}]= [${concat(arr5, arr6)}]`);  //[1,2,3,uno,dos,tres]
 
 // Opcional
 // Implementa una versión del ejercicio anterior donde se acepten múlples arrays de
@@ -69,7 +62,7 @@ const concatMore = (...args) => {
 };
 
 const arr7 = [false, true];
-console.log(`2.-Concat of [${arr5}]+[${arr6}]+[${arr7}]= [${concatMore(arr5, arr6, arr7)}]`);
+console.log(`2.-Concat of [${arr5}]+[${arr6}]+[${arr7}]= [${concatMore(arr5, arr6, arr7)}]`); //[1,2,3,uno,dos,tres,false,true]
 
 // 3. Clone Merge
 // Implementa una función clone que, a partir de un objeto de entrada source devuelva un nuevo objeto con las propiedades de source:
@@ -84,7 +77,7 @@ function clone(source) {
     return properties
 }
 
-console.log(`3.-Properties of ${JSON.stringify(source)} are: ${clone(source)}`);
+console.log(`3.-Properties of ${JSON.stringify(source)} are: ${clone(source)}`);  //name,age,type
 
 // Merge
 // Implementa una función merge que, dados dos objetos de entrada source y target ,
@@ -98,13 +91,13 @@ const target = {
     color: "white"
 }
 
-function merge(source, target) {
+const merge = (source, target) => {
     const newObj = {...target, ...source }
     return newObj;
 }
 
 //   merge(source, target)
-console.log("3.-Merged new object is: ", merge(source, target));
+console.log("3.-Merged new object is: ", merge(source, target)); //{ name: 'Eric', age: 34, type: 'human', color: 'white' }
 
 // 4. Read Books
 // (ver archivo ejercicio4.ts)
