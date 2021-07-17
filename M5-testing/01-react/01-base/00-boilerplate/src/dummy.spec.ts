af1cccc794933718b0463e2430ab05bab844aa96
ercardona <ercardona@edrans.com>
2021-07-17 18:37:21 +0200

    tests

---
+++ M5-testing/01-react/01-base/00-boilerplate/src/dummy.spec.ts
@@ -0,0 +1,18 @@
+describe('dummy, specs', () => {
+  it('should pass spec', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    expect(true).toBeTruthy();
+  });
+  it('should fail spec', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+    expect(true).toBeFalsy();
+  });
+});
